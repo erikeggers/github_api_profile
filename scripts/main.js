aa355d9f153d666e569b2350dd6382f2b90ff230
Erik Eggers <em.eggers@gmail.com>
2015-01-27 23:26:53 -0500

    Added some more elements from ajax

--- scripts/main.js
+++ scripts/main.js
@@ -13,19 +13,15 @@ $(document).ready(function() {
 
   //sidebar
   $.ajax({
-    url: "https://api.github.com/users/erikeggers" + gitHubT,
-    type: 'GET',
-    dataType: 'json',
+    url: "https://api.github.com/users/erikeggers" + gitHubT
   }).done(function(userData) {
-    console.log(userData);
+    // console.log(userData);
     $sidebarOutput.append(renderSidebarTemplate(userData));
   });
 
   //repo
   $.ajax({
-    url: "https://api.github.com/users/erikeggers/repos" + gitHubT,
-    type: 'GET',
-    dataType: 'json',
+    url: "https://api.github.com/users/erikeggers/repos" + gitHubT
   }).done(function(userData) {
     console.log(userData);
     _.each(userData, function(item) {
@@ -35,32 +31,7 @@ $(document).ready(function() {
       console.log(data);
       $repoOutput.append(renderRepoTemplate(data));
     });
-    // $repoOutput.append(renderSidebarTemplate(userData));
   });
-
-    // $output.append()
-
-    // sign up for an api key
-    // go to your settings
-    // application
-
-    // generate a new token
-    // type in password
-    // repo, public repo & user
-    // generate token ... do not push to github
-    // grab your token
-    // copy it!!!!
-    // in your project create a javascript file just for your token
-    // token.js
-    // window.githubToken = "paste the string you copied"
-    // save the file
-    // .gitignore token.js
-    // link to the file in your html at the end of your body
-    // in main.js, include token when you make ajax calls
-    // access_token=OAUTH-
-    // $.ajax({
-    // url: githubURL + "&access_token=" + githubToken
-    // })
 
  });
 })();
