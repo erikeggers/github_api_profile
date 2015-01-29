7e83093dc1e86612a3435cecfe88617dcf173f60
Erik Eggers <em.eggers@gmail.com>
2015-01-29 01:40:55 -0500

    Almost finshed with styles

--- scripts/main.js
+++ scripts/main.js
@@ -2,37 +2,44 @@
   'use strict';
 
 var baseUrl = "https://api.github.com/users/erikeggers";
-var gitHubT = "?access_token=" + token;
+// var gitHubT = "?access_token=" + token;
 
 $(document).ready(function() {
+
+  if(typeof token !== 'undefined'){
+      $.ajaxSetup({
+        headers: { 'Authorization': 'token ' + token }
+      });
+    }
 
   var $userInfoOutput = $('.user-info');
   var $orgsOutput = $('.orgs');
   var $repoOutput = $('.repo-container');
 
+  //templates
   var renderUserTemplate = _.template($('.user-items').text());
   var renderOrgsTemplate = _.template($('.user-orgs').text());
   var renderRepoTemplate = _.template($('.repo-info').text());
 
   //sidebar
-  $.ajax( baseUrl + gitHubT).done(function(userData) {
+  $.ajax(baseUrl).done(function(userData) {
     $userInfoOutput.append(renderUserTemplate(userData));
   });
 
   //sidebar-starred
-  $.ajax(baseUrl + '/starred' + gitHubT).done(function(starredData) {
+  $.ajax(baseUrl + '/starred').done(function(starredData) {
     $('.starred-count').text(starredData.length);
   });
 
   //sidebar-orgs
-  $.ajax(baseUrl + '/orgs' + gitHubT).done(function(userOrgs) {
+  $.ajax(baseUrl + '/orgs').done(function(userOrgs) {
     _.each(userOrgs, function(item) {
     $orgsOutput.append(renderOrgsTemplate(item));
     });
   });
 
   //repo
-  $.ajax(baseUrl + '/repos' + gitHubT).done(function(userRepos) {
+  $.ajax(baseUrl + '/repos').done(function(userRepos) {
     _.each(userRepos, function(item) {
       $repoOutput.append(renderRepoTemplate(item));
   });
